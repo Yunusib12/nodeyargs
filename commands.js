2a44577fda3278978612422a9b353adb40610a67
Ibrahim Yunus <younis.ib@gmail.com>
2020-05-23 16:53:36 -0400

    Updated with promise based reading file

--- commands.js
+++ commands.js
@@ -1,37 +1,36 @@
 const fs = require('fs');
 const chalk = require('chalk');
+const util = require('util');
 
 const create = (fileName) => {
 
     let sentence = "You are awesome";
     let fileNameArray = [];
     let hasMatch = false;
+    let read = util.promisify(fs.readFile);
 
-    //Checking if the file Name exist
-    try {
+    Promise.all([read('./datas/filenamedb.json')])
+        .then((datas) => {
 
-        fileNameArray = JSON.parse(fs.readFileSync('./datas/filenamedb.json'));
-        hasMatch = fileNameArray.find((x) => x.fileName === fileName);
+            fileNameArray = JSON.parse(datas);
+            hasMatch = fileNameArray.find((x) => x.fileName === fileName);
 
-    } catch (error) {
-        //console.log(error);
-    }
+            if (hasMatch) {
+                console.log(chalk.red(`A file with the name " ${chalk.underline.red(fileName)} " exist! Please choose another one.`));
+            } else {
+                //Adding the file Name into the filenamedb.json
+                fileNameArray.push({ fileName });
+                fs.writeFile('./datas/filenamedb.json', JSON.stringify(fileNameArray), (error) => {
+                    if (error) throw error
+                });
 
-    if (hasMatch) {
-        console.log(chalk.red(`A file with the name " ${chalk.underline.red(fileName)} " exist! Please choose another one.`));
-    } else {
-        //Adding the file Name into the filenamedb.json
-        fileNameArray.push({ fileName });
-        fs.writeFile('./datas/filenamedb.json', JSON.stringify(fileNameArray), (error) => {
-            if (error) throw error
-        });
-
-        //Creating the file in the system
-        fs.writeFile(`./datas/${fileName}`, sentence, (err) => {
-            if (err) throw err
-            console.log(chalk.green(`The File " ${chalk.green.underline(fileName)} " was created successfuly!`));
-        });
-    }
+                //Creating the file in the system
+                fs.writeFile(`./datas/${fileName}`, sentence, (err) => {
+                    if (err) throw err
+                    console.log(chalk.green(`The File " ${chalk.green.underline(fileName)} " was created successfuly!`));
+                });
+            }
+        }).catch((err) => { console.log(err) });
 };
 
 module.exports = {
